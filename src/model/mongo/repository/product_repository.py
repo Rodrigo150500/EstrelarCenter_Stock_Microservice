1177a25b7f081175b2404b818e7c922d6ba07f27
Rodrigo150500 <rodrigo.takara1505@gmail.com>
2025-11-26 17:18:11 -0300

    Feat: Implementing products mongo repository

---
+++ src/model/mongo/repository/product_repository.py
@@ -0,0 +1,97 @@
+import os
+from pymongo.results import DeleteResult, UpdateResult
+
+
+COLLECTION_NAME = os.getenv("COLLECTION_NAME_MONGO_DB_PRODUCTS")
+
+class ProductRepository:
+
+    def __init__(self, connection):
+        
+        self.__collection = connection.get_collection(COLLECTION_NAME)
+    
+
+    def get_product_by_code(self, code: str) -> dict:
+
+        product_filter = {
+            code:{"$exists": True}
+        }
+
+        #Se não houver encontrado retorno é none
+        product =  self.__collection.find_one(product_filter)
+        
+        return product
+    
+    
+    def remove_item(self, code: str, item: int) -> UpdateResult:
+
+        product_filter = {code: {"$exists": True}}
+
+        # remove pelo índice
+        self.__collection.update_one(
+            product_filter,
+            {"$unset": {f"{code}.{item}": item}}
+        )
+        
+        # 2. depois remove os "null" do array
+        response = self.__collection.update_one(
+            product_filter,
+            {"$pull": {code: None}}
+        )
+
+        return response
+    
+
+    def delete_product_by_code(self, code: str) -> DeleteResult:
+
+        product_filter = {
+            code:{"$exists": True}
+        }
+
+        response = self.__collection.delete_one(
+        product_filter)
+
+        return response
+    
+
+    def insert_product_item(self, code: str, fields: dict) -> UpdateResult:
+
+        product_filter = {code: {"$exists": True}}
+
+        update_query = {
+            "$push": {
+                code: fields
+            }
+        }
+
+        response = self.__collection.update_one(
+            product_filter,
+            update_query,
+            upsert=True
+        )
+
+        return response
+
+
+    def update_product_item(self, code: str, item: int, fields: dict) -> UpdateResult:
+
+        product_filter = {f"{code}.{item}": {"$exists": True}}
+
+        update_query = {
+            "$set": {
+                f"{code}.{item}": fields
+            }
+        }
+
+        response = self.__collection.update_one(product_filter, update_query)
+
+        return response
+
+
+    def get_all_products(self) -> list:
+        
+        products = self.__collection.find({})
+        
+        products = [document for document in products]
+
+        return products
