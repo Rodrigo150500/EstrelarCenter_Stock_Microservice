c5e46a5795b9653c13023e35b8b68f7445865da7
Rodrigo150500 <rodrigo.takara1505@gmail.com>
2025-11-27 17:31:13 -0300

    Feat: Implementing integration tests with mongo database

---
+++ src/model/mongo/repository/data/product_repository_integration_data.py
@@ -0,0 +1,30 @@
+def insert_product_item_data():
+
+    fields = {
+            "code": "10",
+            "description": "Product A",
+            "brand": "brand",
+            "reference": "IMA",
+            "location": "C06",
+            "last_change": "-3 23/09/2025 17:17:27",
+            "stock": 5,
+            "measure":"Unidade"
+            }
+
+    return fields
+
+
+def update_product_item_data():
+     
+    fields = {
+            "code": "10",
+            "description": "Product kkkk",
+            "brand": "brand 1",
+            "reference": "IMA 1",
+            "location": "C07",
+            "last_change": "-3 23/09/2025 17:17:27",
+            "stock": 25,
+            "measure":"Unidade"
+            }
+
+    return fields
---
+++ src/model/mongo/repository/product_repository_integration_test.py
@@ -0,0 +1,93 @@
+from dotenv import load_dotenv
+load_dotenv("dev.env")
+
+import pytest
+from src.model.mongo.settings.mongo_db_connection import mongo_db_connection
+from .product_repository import ProductRepository
+
+from .data.product_repository_integration_data import insert_product_item_data, update_product_item_data
+
+mongo_db_connection.connect()
+
+
+@pytest.fixture
+def setup_repository():
+
+    connection = mongo_db_connection.get_db_connection()
+    
+    repository = ProductRepository(connection)
+
+    return repository
+
+
+@pytest.mark.skip()
+def test_insert_product_item(setup_repository):
+
+    data = insert_product_item_data()
+
+    repository = setup_repository
+
+    response = repository.insert_product_item("10", data)
+    
+    assert response.matched_count == 0
+    assert response.modified_count == 0
+    assert response.upserted_id is not None
+
+
+@pytest.mark.skip()
+def test_insert_new_item_in_product_exist(setup_repository):
+
+    data = insert_product_item_data()
+
+    repository = setup_repository
+
+    response = repository.insert_product_item("10", data)
+    
+    assert response.matched_count == 1
+    assert response.modified_count == 1
+
+
+@pytest.mark.skip()
+def test_get_all_products(setup_repository):
+
+    repository = setup_repository
+
+    response = repository.get_all_products()
+
+    assert len(response) > 0
+
+
+@pytest.mark.skip()
+def test_update_product_item(setup_repository):
+
+    data = update_product_item_data()
+
+    repository = setup_repository
+
+    response = repository.update_product_item("10",1, data)
+
+    assert response.matched_count == 1
+    assert response.modified_count == 1
+
+
+@pytest.mark.skip()
+def test_remove_item(setup_repository):
+
+    repository = setup_repository
+
+    response = repository.remove_item("10",2)
+
+    assert response.matched_count == 1
+    assert response.modified_count == 1
+
+
+@pytest.mark.skip()
+def test_remove_product(setup_repository):
+
+    repository = setup_repository
+
+    response = repository.delete_product_by_code("10")
+
+    assert response.deleted_count == 1
+
+
