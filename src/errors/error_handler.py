484fc852dc2e4f08f79796d2e2015e26b6b954c4
Rodrigo150500 <rodrigo.takara1505@gmail.com>
2025-11-29 15:26:46 -0300

    Feat: Extend error handler to include HttpNotFound and HttpInternalServerError

--- src/errors/error_handler.py
+++ src/errors/error_handler.py
@@ -1,11 +1,14 @@
 from .types.http_unavailable_service import HttpUnavailableService
 from .types.http_unprocessable_entity import HttpUnprocessableEntity
+from .types.http_not_found import HttpNotFound
+from .types.http_internal_server_error import HttpInternalServerError
+
 
 from src.main.http_types.http_response import HttpResponse
 
 def error_handler(error):
 
-    if(isinstance(error, (HttpUnavailableService, HttpUnprocessableEntity))):
+    if(isinstance(error, (HttpUnavailableService, HttpUnprocessableEntity, HttpNotFound, HttpInternalServerError))):
 
         return HttpResponse(
             body={
