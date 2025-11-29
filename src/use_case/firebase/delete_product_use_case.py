98b33926d3827a69e8d845aa37dbe077d855ed38
Rodrigo150500 <rodrigo.takara1505@gmail.com>
2025-11-29 15:27:03 -0300

    Feat: Enhance delete product use case with improved error handling and response status

--- src/use_case/firebase/delete_product_use_case.py
+++ src/use_case/firebase/delete_product_use_case.py
@@ -6,6 +6,9 @@ from src.main.http_types.http_request import HttpRequest
 from src.validators.delete_product_validator_request import delete_product_validator_request
 
 from src.errors.types.http_not_found import HttpNotFound
+from src.errors.types.http_unprocessable_entity import HttpUnprocessableEntity
+from src.errors.types.http_unavailable_service import HttpUnavailableService
+from src.errors.types.http_internal_server_error import HttpInternalServerError
 
 from .interfaces.delete_product_use_case_interface import DeleteProductUseCaseInterface
 
@@ -27,28 +30,47 @@ class DeleteProductFirebaseUseCase(DeleteProductUseCaseInterface):
         self.__delete_product_in_database(params)
 
         formatted_response = self.__format_response(params)
- 
+
         return formatted_response
 
 
     def __verify_if_exists_in_database(self, params: dict) -> None:
 
-        code = params["code"]
+        try:
+            code = params["code"]
 
-        product = self.__repository.get_product_by_code(code)
+            product = self.__repository.get_product_by_code(code)
 
-        if (not product):
+            if (not product):
 
-            raise HttpNotFound("Product not found")
+                raise HttpNotFound("Product not found")
+        
+        except HttpNotFound:
+
+            raise
+        
+        except Exception as exception:
+
+            print(f"Error:[DeleteProductFirebaseUseCase][__verify_if_exists_in_database]: {str(exception)}")
+            
+            raise HttpUnavailableService("Error: Banco de dados indisponível")
     
 
     def __delete_product_in_database(self, params):
 
-        code = params["code"]
+        try:
 
-        self.__repository.delete_product_by_code(code)
-    
+            code = params["code"]
 
+            self.__repository.delete_product_by_code(code)
+
+        except Exception as exception:
+            
+            print(f"Error:[DeleteProductFirebaseUseCase][__delete_product_in_database]: {str(exception)}")
+            
+            raise HttpUnavailableService("Erro ao deletar no banco de dados")
+
+        
     def __format_response(self, params: dict) -> HttpResponse:
 
         return HttpResponse(
@@ -58,5 +80,7 @@ class DeleteProductFirebaseUseCase(DeleteProductUseCaseInterface):
                     "count": 1,
                     "attributes": params
                 }
-            }, status_code=204
+            }, status_code=200
         )
+        
+     
