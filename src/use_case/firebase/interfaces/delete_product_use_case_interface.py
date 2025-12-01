2ffd2aca6947906bacb72ac14244c9ce391886a0
Rodrigo150500 <rodrigo.takara1505@gmail.com>
2025-12-01 09:47:33 -0300

    Add: Adding interface for use_case firebase

---
+++ src/use_case/firebase/interfaces/delete_product_use_case_interface.py
@@ -0,0 +1,11 @@
+from abc import ABC, abstractmethod
+
+from src.main.http_types.http_request import HttpRequest
+from src.main.http_types.http_response import HttpResponse
+
+
+class DeleteProductUseCaseInterface(ABC):
+
+    @abstractmethod
+    def handle(self, http_request: HttpRequest) -> HttpResponse:
+        pass
---
+++ src/use_case/firebase/interfaces/update_product_use_case_interface.py
@@ -0,0 +1,11 @@
+from abc import ABC, abstractmethod
+
+from src.main.http_types.http_request import HttpRequest
+from src.main.http_types.http_response import HttpResponse
+
+
+class UpdateProductUseCaseInterface(ABC):
+
+    @abstractmethod
+    def handle(self, http_request: HttpRequest) -> HttpResponse:
+        pass
