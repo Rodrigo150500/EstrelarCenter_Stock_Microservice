991bf08809c5d04faca1a571018158d322946391
Rodrigo150500 <rodrigo.takara1505@gmail.com>
2025-12-03 18:09:43 -0300

    Feat: Add HttpConflict exception class for handling HTTP 409 conflicts

---
+++ src/errors/types/http_conflict.py
@@ -0,0 +1,8 @@
+class HttpConflict(Exception):
+
+    def __init__(self, message: str) -> None:
+        super().__init__(message)
+
+        self.message = message
+        self.status_code = 409
+        self.name = "HttpConflict"
