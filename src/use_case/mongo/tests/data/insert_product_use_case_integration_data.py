78c055bcb992284e6bfa37a457205d7f87862dd5
Rodrigo150500 <rodrigo.takara1505@gmail.com>
2025-12-18 17:01:55 -0300

    Refact: Refact test use case integration data for new data structurure

--- src/use_case/mongo/tests/data/insert_product_use_case_integration_data.py
+++ src/use_case/mongo/tests/data/insert_product_use_case_integration_data.py
@@ -1,39 +1,45 @@
+from unittest.mock import ANY
+
 from src.utils.export_image_string64_to_binary import export_image_string64_to_binary
+
+from src.utils.image_type import image_string
 
 def insert_product_sucessfully():
 
-    imagem_bytes = bytes([
-        0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A,
-        0x00, 0x00, 0x00, 0x0D, 0x49, 0x48, 0x44, 0x52,
-        0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x01,
-        0x08, 0x02, 0x00, 0x00, 0x00, 0x90, 0x77, 0x53,
-        0xDE, 0x00, 0x00, 0x00, 0x0A, 0x49, 0x44, 0x41,
-        0x54, 0x08, 0xD7, 0x63, 0x60, 0x00, 0x00, 0x00,
-        0x02, 0x00, 0x01, 0xE2, 0x26, 0x05, 0x9B, 0x00,
-        0x00, 0x00, 0x00, 0x49, 0x45, 0x4E, 0x44, 0xAE,
-        0x42, 0x60, 0x82
-    ])
-
-
     body = {
-            'code': "16",
-            'description': "Product A" ,
-            'measure': "Unidade",
-            'stock': 25,
-            'brand': "Kit-Kat",
-            'last_change': "+5 03/12/25 12:35:33"  ,
-            'image': imagem_bytes,
-            'location': "P9",
-            'reference': "KIT104",
+        "code": "10",
+        "description": "Chocolate kit-kat 41g",
+        "stock": 15,
+        "image": image_string,
+        "brand": "kit-kat",
+        "reference": "kit-154",
+        "location": "CX15",
+        "measure": "Caixa",
+        "keepBuying": True
     }
 
-    body["image"] = export_image_string64_to_binary(body['image'])
+
+    body_response = {
+        "_id": ANY,
+        "code": "10",
+        "variants":[{
+            "description": "Chocolate kit-kat 41g",
+            "stock": 15,
+            "image": export_image_string64_to_binary(image_string),
+            "brand": "kit-kat",
+            "reference": "kit-154",
+            "location": "CX15",
+            "measure": "Caixa",
+            "keepBuying": True,
+            "last_change": ANY
+        }]
+    }
 
     expected_body = {
         "data":{
             "operation": "Insert",
             "count": 1,
-            "attributes": body
+            "attributes": body_response
         }
     }
     
@@ -48,15 +54,15 @@ def insert_product_sucessfully():
 def insert_product_that_already_exists():
 
     body = {
-            'code': "16",
-            'description': "Product A" ,
-            'measure': "Unidade",
-            'stock': 25,
-            'brand': "Kit-Kat",
-            'last_change': "+5 03/12/25 12:35:33"  ,
-            'image': "imagem_bytes",
-            'location': "P9",
-            'reference': "KIT104",
+        "code": "10",
+        "description": "Chocolate kit-kat 41g",
+        "stock": 15,
+        "image": image_string,
+        "brand": "kit-kat",
+        "reference": "kit-154",
+        "location": "CX15",
+        "measure": "Caixa",
+        "keepBuying": True
     }
 
    
