7f6fd7388b87da2bd4597bdadb36772397288e73
Rodrigo150500 <rodrigo.takara1505@gmail.com>
2025-12-27 17:37:48 -0300

    Feat: Implementing update_validation_request with tests

---
+++ src/validators/tests/update_product_validator_request_test.py
@@ -0,0 +1,77 @@
+import pytest
+
+from src.errors.types.http_unprocessable_entity import HttpUnprocessableEntity
+
+from src.validators.update_product_validator_request import update_product_validator_request
+
+from src.utils.image_type import image_string
+
+def test_validate_update_sucessfully():
+
+    params = {
+        "code": "123",
+        "index": "0"
+    }
+
+    body = {
+        "code": "123",
+        "description": "Product A",
+        "stock": 15,
+        "image": image_string,
+        "brand": "Kit-Kat",
+        "reference": "Kit500",
+        "location": "CX15",
+        "measure": "Unidade",
+        "keepBuying": False,
+        "quantity_change": -10   
+    }
+
+    update_product_validator_request(body, params)
+
+
+def test_validate_wrong_input_type_return_error():
+
+    params = {
+        "code": "123",
+        "index": 1 #must be string 
+    }
+
+    body = {
+        "code": 123, #Must be string
+        "description": "Product A",
+        "stock": 15,
+        "image": image_string,
+        "brand": "Kit-Kat",
+        "reference": "Kit500",
+        "location": "CX15",
+        "measure": "Unidade",
+        "keepBuying": "False", #Must be boolean
+        "quantity_change": -10   
+    }
+
+    with pytest.raises(HttpUnprocessableEntity):
+
+        update_product_validator_request(body, params)
+
+
+def test_not_filling_body_return_sucessfully():
+
+    params = {
+        "code": "123",
+        "index": "1" 
+    }
+
+    body = {
+        "code": "123",
+        "description": "",
+        "stock": 0,
+        "image": "",
+        "brand": "",
+        "reference": "",
+        "location": "",
+        "measure": "",
+        "keepBuying": False, 
+        "quantity_change": 0   
+    }
+
+    update_product_validator_request(body, params)
--- src/validators/update_product_validator_request.py
+++ src/validators/update_product_validator_request.py
@@ -2,25 +2,30 @@ from src.errors.types.http_unprocessable_entity import HttpUnprocessableEntity
 from cerberus import Validator
 
 
-def update_product_validator_request(body: dict):
+def update_product_validator_request(body: dict, params: dict):
 
-  body_validate = Validator({         
-            'code':{'type': 'string', 'required': True},
-            'description': {'type': 'string', 'required': True},
-            'measure': {'type': 'string', 'required': True},
-            'stock': {'type': 'integer', 'required': True},
-            'brand': {'type': 'string', 'required': False},
-            'last_change': {'type': 'string', 'required': False},
-            'image': {'type': 'string', 'required': False},
-            'location': {'type': 'string', 'required': False},
-            'reference': {'type': 'string', 'required': False},
-            'item': {"type":"integer", "required": True}       
+  params_validate = Validator({
+    "code": {"type": "string", "required": True, "empty": False},
+    "index": {"type": "string", "required": True, "minlength": 1}
   })
 
-  response = body_validate.validate(body)
+  body_validate = Validator({         
+    "code":{'type': 'string', 'required': True, "empty": False},
+    "description": {"type": "string", "required": False},
+    "stock": {"type": "integer", "required": False},
+    "image": {"type": "string", "required": False},
+    "brand": {"type": "string", "required": False},
+    "reference": {"type": "string", "required": False},
+    "location": {"type": "string", "required": False},
+    "measure": {"type": "string", "required": False},
+    "keepBuying": {"type": "boolean", "required": False},
+    "quantity_change":{"type": "integer", "required": False}     
+  })
 
+  response_body = body_validate.validate(body)
+  response_params = params_validate.validate(params)
 
-  if response is False:
+  if response_body is False:
     error = body_validate.errors
     error_key_message = list(error.keys())[0]
     error_message = error[error_key_message]
@@ -30,4 +35,17 @@ def update_product_validator_request(body: dict):
     raise HttpUnprocessableEntity(
       message=formatted_error_message
     )
+  
+  if response_params is False:
+    error = params_validate.errors
+    error_key_message = list(error.keys())[0]
+    error_message = error[error_key_message]
+
+    formatted_error_message = f"Erro no campo {error_key_message}\n{error_message}"
+
+    raise HttpUnprocessableEntity(
+      message=formatted_error_message
+    )
+  
+
 
