9e5a49c1b468b1273a537c6e8d1431572c8f31a7
Rodrigo150500 <rodrigo.takara1505@gmail.com>
2026-01-31 18:12:11 -0300

    Fix: Update HOST variable usage in export_image_with_error function

--- src/utils/tests/data/export_image_string64_to_binary_data.py
+++ src/utils/tests/data/export_image_string64_to_binary_data.py
@@ -1,6 +1,7 @@
 import os
 
 PORT = os.getenv("PORT")
+HOST = os.getenv("HOST")
 
 
 def export_image_sucessfully():
@@ -32,7 +33,7 @@ def export_image_with_error():
 
     image = "image"
 
-    expected_response = f"https://localhost:{PORT}/static/stock/src/assets/erro.jpg" 
+    expected_response = f"https://{HOST}:{PORT}/static/erro.jpg" 
     
 
     data = {
