624496da07b9b442238e61764b5b886d8c0fa117
Rodrigo150500 <rodrigo.takara1505@gmail.com>
2026-02-06 09:25:09 -0300

    Fix: Refactor HttpResponse constructor to accept status_code first and allow body to be optional

--- src/main/http_types/http_response.py
+++ src/main/http_types/http_response.py
@@ -1,6 +1,4 @@
 class HttpResponse:
-
-    def __init__(self, body: str, status_code: int):
-        
-        self.body = body
+    def __init__(self, status_code: int, body: dict | None = None):
         self.status_code = status_code
+        self.body = body
