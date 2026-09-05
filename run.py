ff796571f2c22686d318e9d6719750890cd57872
Rodrigo150500 <rodrigo.takara1505@gmail.com>
2026-09-05 12:33:59 -0300

    Fix: adding CERT and KEY for certified

--- run.py
+++ run.py
@@ -9,7 +9,8 @@ app = create_app()
 
 HOST = os.getenv("HOST")
 PORT = os.getenv("PORT")
-
+CERT = os.getenv("SSL_CERT")
+KEY = os.getenv("SSL_KEY")
 
 if __name__ == "__main__":
-    app.run(debug=True, host=HOST, port=PORT)
+    app.run(debug=True, host=HOST, port=PORT, ssl_context=(CERT, KEY))
